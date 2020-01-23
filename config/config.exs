fd7c7fd2712d36c98c487f150417753b5de4849f
Manzanit0 <unico.javier@gmail.com>
2020-01-23 13:24:31 +0000

    Disable hackney pooling to avoid :checkout_timeout errors

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,7 @@
 import Config
 
+# TODO added this due to getting `{:error, %HTTPoison.Error{id: nil, reason: :checkout_timeout}}`
+# however, this should be worked around by pooling the requests instead of parallelising all of them.
+config :hackney, use_default_pool: false
+
 import_config "#{Mix.env()}.exs"
